7c1e3a98e9b41b6f5a0b5c541d1b2e110c2fb6fe
Karl Sixten Bjarnø <karlsixten@gmail.com>
2025-03-14 11:30:01 +0100

    Added options for constructing pages

--- 06_DogInder/util/pages.js
+++ 06_DogInder/util/pages.js
@@ -4,4 +4,8 @@ const frontpage = readPage("./public/pages/frontpage/frontpage.html");
 export const frontpagePage = constructPage(frontpage);
 
 const matches = readPage("./public/pages/matches/matches.html");
-export const matchesPage = constructPage(matches);
+export const matchesPage = constructPage(matches, 
+{
+    title: 'DogInder | Matches',
+    cssLinks: '<link rel="stylesheet" href="/pages/matches/matches.css">'
+})
