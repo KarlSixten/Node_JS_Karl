5d779d02278c35406ad8d227314cfa70e26c80dc
Karl Sixten Bjarnø <karlsixten@gmail.com>
2025-03-14 10:38:58 +0100

    Added template engine along with templates and pages

--- 06_DogInder/app.js
+++ 06_DogInder/app.js
@@ -1,27 +1,27 @@
 import express from 'express';
 
+import { frontpagePage, matchesPage } from './util/pages.js';
+
 const app = express();
+
+app.use(express.static("public"));
 
 import path from 'path';
 
 import { getMatches } from './util/matches.js'
 
-app.use(express.static("public"));
-
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve('public/frontpage/frontpage.html'));
+    res.send(frontpagePage);    
 });
 
 app.get("/matches", (req, res) => {
-    res.sendFile(path.resolve('public/matches/matches.html'));
+    res.send(matchesPage);
 })
 
 app.get("/api/matches", async (req, res) => {
     const matches = await getMatches();
     res.send({ data : matches });
 })
-
-console.log(process.env.PORT);
 
 const PORT = Number(process.env.PORT) || 8080;
 const server = app.listen(PORT, () => console.log("Server is running on port", server.address().port));
