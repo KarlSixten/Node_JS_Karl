3dd5981d4b1c56c9e351ee438f4a36a28885f116
Karl Sixten Bjarnø <karlsixten@gmail.com>
2025-03-21 10:01:51 +0100

    Added first Vite Svelte project

---
+++ 07_first_Svelte_Project/vite.config.js
@@ -0,0 +1,10 @@
+// Generel config af Vite
+// Instantierer Svelte
+
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+})
