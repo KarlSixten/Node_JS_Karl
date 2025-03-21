9ba5f55ab379d75d8a5253773606cdd5aa80af77
Karl Sixten Bjarnø <karlsixten@gmail.com>
2025-03-21 11:12:09 +0100

    Changes

---
+++ 07_first_Svelte_Project/src/components/Messages/messages.svelte
@@ -0,0 +1,22 @@
+<script>
+    const messages = $state([1, 2, 3, "asd"]);
+
+    // Ville ikke fungerer fordi messages ikke bliver reinstantieret i addMessage().
+    // Brug state som ovenover for at holde subscribe på mutability
+    //const messages = [1, 2, 3, "asd"];
+
+    let messageInput = $state("");
+
+    function addMessage() {
+        messages.push(messageInput);
+        messageInput = "";
+    }
+</script>
+
+<h1>Messages</h1>
+
+<p>{messages}</p>
+
+<input bind:value={messageInput} placeholder="Type your message...">
+
+<button onclick="{addMessage}">Submit message</button>
